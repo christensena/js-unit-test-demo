9831b1eedb6c30cf6311f9450b7d41946cac6fbb
Alan Christensen <alan@christensen.org.nz>
2013-04-18 22:27:55 +1200

    added another example page to unit test

---
+++ examples/row-manager.js
@@ -0,0 +1,25 @@
+(function ($) {
+    // ... all vars and functions are in this scope only
+    // still maintains access to all globals
+    // see http://www.adequatelygood.com/JavaScript-Module-Pattern-In-Depth.html
+
+    function wireUpRowManager(itemListEl, addButtonEl, removeButtonEl, rowCountEl) {
+        addButtonEl.on('click', function(e) {
+            itemListEl.append('<li><input type="text" name="value"/></li>');
+            updateRowCount(itemListEl, rowCountEl);
+        });
+
+        removeButtonEl.on('click', function(e) {
+            itemListEl.find('li:last').remove();
+            //updateRowCount(itemListEl, rowCountEl);
+        });
+    }
+
+    function updateRowCount(itemListEl, rowCountEl) {
+        rowCountEl.text(itemListEl.find('li').length);       
+    }
+
+    // expose to the world
+    window.wireUpRowManager = wireUpRowManager;
+
+}(jQuery));
